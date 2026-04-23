224c3650e62884c2e909aef48d1f668915a79146
tuananh0908123 <tuananh09082004@gmail.com>
2026-04-23 15:49:44 +0700

    FIX

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Full-Stack App/i);
+  const linkElement = screen.getByText(/Hệ Thống Quản Lý Dự Án/i);
   expect(linkElement).toBeInTheDocument();
 });
